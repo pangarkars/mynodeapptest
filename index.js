002a82942d247e97eb0c978316a3ff561bcafbe5
Saurabh <sp.codeio@gmail.com>
2022-03-18 17:04:59 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+const { pool } = require ('./config')
+
+const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(cors())
+
+const getBooks = (request, response) => {
+    pool.query('SELECT * FROM books', (error, results) => {
+        if (error){
+            throw error
+        }
+        response.status(200).json(results.rows)
+    })
+}
+
+const addBook = (request, response) => {
+    const { author, title } = request.body
+    
+    pool.query(
+        'INSERT INTO books (author, title) VALUES ($1, $2)',
+        [author, title],
+        (error) => {
+            if (error){
+                throw error
+            }
+            response.status(201).json({ status: 'success', message: 'Book Added.' })
+        }
+    )
+}
+
+app
+    .route('/books')
+    .get(getBooks)
+    .post(addBook)
+
+app.listen(process.env.PORT || 3002, () => {
+    console.log(`Server listening`)
+})
